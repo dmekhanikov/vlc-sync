dd62113b818ae72cd6bba1068585811fb978eb6e
Denis Mekhanikov <dmekhanikov@gmail.com>
2017-01-29 17:19:01 +0800

    add video manipulation interface (for debug)

--- playback_sync.lua
+++ playback_sync.lua
@@ -5,19 +5,59 @@ function descriptor()
         capabilities = {} }
 end
 
+widgets = {
+    dialog = nil,
+    host_input = nil,
+    port_input = nil,
+    connect_button = nil,
+    disconnect_button = nil,
+
+    play_button = nil,
+    seek_input = nil,
+    seek_button = nil
+}
+
+socket = nil
+
 function activate()
-    dialog = vlc.dialog("Playback Synchronization")
-    dialog:add_label("Host:", 1, 1)
-    dialog:add_label("Port:", 2, 1)
-    host_input = dialog:add_text_input("", 1, 2)
-    port_input = dialog:add_text_input("7773", 2, 2)
-    connect_button = dialog:add_button("Connect", connect, 3, 2)
-    dialog:show()
+    widgets.dialog = vlc.dialog("Playback Synchronization")
+    widgets.dialog:add_label("Host:", 1, 1)
+    widgets.dialog:add_label("Port:", 2, 1)
+    widgets.host_input = widgets.dialog:add_text_input("", 1, 2)
+    widgets.port_input = widgets.dialog:add_text_input("7773", 2, 2)
+    widgets.connect_button = widgets.dialog:add_button("Connect", connect, 3, 2)
+
+
+    widgets.dialog:add_button("Play", vlc.playlist.play, 1, 3)
+    widgets.dialog:add_button("Pause", vlc.playlist.pause, 2, 3)
+    widgets.seek_input = widgets.dialog:add_text_input("", 1, 4)
+    widgets.seek_button = widgets.dialog:add_button("Seek",
+        function() seek(widgets.seek_input:get_text()) end,
+        2, 4)
+    widgets.dialog:show()
 end
 
 function connect()
-    local host = host_input:get_text()
-    local port = port_input:get_text()
+    local host = widgets.host_input:get_text()
+    local port = widgets.port_input:get_text()
+    -- connect
+    vlc.msg.info("Connected to " .. host .. ":" .. port)
+    widgets.dialog:del_widget(widgets.connect_button)
+    widgets.connect_button = nil
+    widgets.disconnect_button = widgets.dialog:add_button("Disconnect", disconnect, 3, 2)
+end
+
+function disconnect()
+    -- disconnect
+    vlc.msg.info("Connection closed")
+    widgets.dialog:del_widget(widgets.disconnect_button)
+    widgets.disconnect_button = nil
+    widgets.connect_button = widgets.dialog:add_button("Connect", connect, 3, 2)
+end
+
+function seek(time)
+    local input = vlc.object.input()
+    vlc.var.set(input, "time", time)
 end
 
 function deactivate()
