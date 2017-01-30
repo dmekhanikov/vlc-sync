1df48ba3b7db6b9c2570e0e38f4360b9a6e6b9c5
Denis Mekhanikov <dmekhanikov@gmail.com>
2017-01-30 11:56:16 +0800

    track play status and send corresponding messages

--- src/main/lua/sync.lua
+++ src/main/lua/sync.lua
@@ -4,8 +4,16 @@ port = 7773
 message_type_size = 1
 message_pos_size = 4
 sleep_duration = 2500
+play_message = string.char(1)
+pause_message = string.char(2)
+
+status = {
+    play_status = false
+}
 
 vlc.msg.info("Sync interface")
+
+socket = vlc.net.connect_tcp(host, port)
 
 function read(fd, length)
     local left = length
@@ -16,9 +24,15 @@ function read(fd, length)
             message = message .. s
             left = left - s:len()
         end
+
         vlc.misc.mwait(vlc.misc.mdate() + sleep_duration)
+        process_changes()
     end
     return message
+end
+
+function write(fd, data)
+    vlc.net.send(fd, data)
 end
 
 function seek(time)
@@ -26,8 +40,33 @@ function seek(time)
     vlc.var.set(input, "time", time)
 end
 
+function process_changes()
+    local play_status = vlc.playlist.status()
+    if status.play_status ~= play_status then
+        if play_status == "playing" then
+            handle_play()
+        else
+            handle_pause()
+        end
+        update_status()
+    end
+end
 
-socket = vlc.net.connect_tcp(host, port)
+function handle_play()
+    vlc.msg.dbg("Sending play message")
+    write(socket, play_message)
+end
+
+function handle_pause()
+    vlc.msg.dbg("Sending pause message")
+    write(socket, pause_message)
+end
+
+function update_status()
+    local play_status = vlc.playlist.status()
+    status.play_status = play_status
+end
+
 --[[
     1 byte: code
     01: play
@@ -49,4 +88,5 @@ while true do
     else
         vlc.msg.dbg("Message of unknown type received")
     end
+    update_status()
 end
